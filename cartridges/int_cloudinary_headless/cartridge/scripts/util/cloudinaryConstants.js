9ae2652be1894d22f8893c858fb3fffd45a6a8ac
Asad Saleem <asaleem@rafter.one>
2024-06-25 19:18:52 +0500

    Update analytics tag

--- cartridges/int_cloudinary_headless/cartridge/scripts/util/cloudinaryConstants.js
+++ cartridges/int_cloudinary_headless/cartridge/scripts/util/cloudinaryConstants.js
@@ -6,7 +6,7 @@ var CloudinaryConstants = module.superModule;
  * Cloudinary Constants sets up various values to be used throughout the cartridge code
  */
 
-CloudinaryConstants.CLD_TRACKING_PARAM = '_i=AO';
+CloudinaryConstants.CLD_TRACKING_PARAM = '_i=AP3';
 CloudinaryConstants.CLD_SFCC_PLATFORM_ARCHITECTURE = 'headless';
 
 module.exports = CloudinaryConstants;

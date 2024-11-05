e5cbc29e266a133f8ae2fd0e46ce5f278385f144
Asad Saleem <asaleem@rafter.one>
2024-11-05 15:51:00 +0500

    update shrink wrap render logic

--- cartridges/int_cloudinary/cartridge/js/cloudinaryWidgets.js
+++ cartridges/int_cloudinary/cartridge/js/cloudinaryWidgets.js
@@ -5,17 +5,24 @@ var $ = require('jquery');
 window.renderCloudinaryGalleryWidget = function () {
     var imgUrls;
     var $cldEl = $('.cloudinary-data-container');
-    var cloudinaryObj = $cldEl.data('cloudinary');
 
-    if (cloudinaryObj) {
-        if (cloudinaryObj.galleryEnabled && typeof cloudinary !== 'undefined') {
-            var galleryOptions = cloudinaryObj.images.galleryWidget.options;
-            window.cldGallery = cloudinary.galleryWidget(galleryOptions); // eslint-disable-line no-undef
-            cldGallery.render(); // eslint-disable-line no-undef
-        } else if (cloudinaryObj.images && cloudinaryObj.images.imageURLs) {
-            imgUrls = cloudinaryObj.images.imageURLs;
+    $cldEl.each(function () {
+        var cloudinaryObj = $(this).data('cloudinary');
+
+        if (cloudinaryObj) {
+            if (cloudinaryObj.galleryEnabled && typeof cloudinary !== 'undefined') {
+                var galleryOptions = cloudinaryObj.images.galleryWidget.options;
+                if (cloudinaryObj.domain !== 'res.cloudinary.com') {
+                    galleryOptions.SecureDistribution = cloudinaryObj.domain;
+                    galleryOptions.privateCdn = true;
+                }
+                window.cldGallery = window.cldProductGallery ? window.cldProductGallery.galleryWidget(galleryOptions) : cloudinary.galleryWidget(galleryOptions); // eslint-disable-line no-undef
+                cldGallery.render(); // eslint-disable-line no-undef
+            } else if (cloudinaryObj.images && cloudinaryObj.images.imageURLs) {
+                imgUrls = cloudinaryObj.images.imageURLs;
+            }
         }
-    }
+    });
 
     return imgUrls;
 };
@@ -30,7 +37,11 @@ window.renderCloudinarySetGalleryWidgets = function () {
 
         if (cldObj && cldObj.isEnabled && cldSetImages && cldSetImages.galleryWidget &&
             cldObj.galleryEnabled && typeof cloudinary !== 'undefined') {
-            window.cldGallery = cloudinary.galleryWidget(cldSetImages.galleryWidget.options); // eslint-disable-line no-undef
+            if (cldObj.domain !== 'res.cloudinary.com') {
+                cldSetImages.galleryWidget.options.SecureDistribution = cldObj.domain;
+                cldSetImages.galleryWidget.options.privateCdn = true;
+            }
+            window.cldGallery = window.cldProductGallery.galleryWidget(cldSetImages.galleryWidget.options); // eslint-disable-line no-undef
             cldGallery.render(); // eslint-disable-line no-undef
         }
     });
@@ -48,7 +59,12 @@ window.renderCloudinaryVideoPlayer = function () {
         if (cldObj && cldObj.video && cldObj.video.videoURL &&
             cldObj.video.videoURL !== '' && cldObj.video.videoURL !== 'null') {
             if (cldObj.videoPlayerEnabled && typeof cloudinary !== 'undefined') {
-                var cld = cloudinary.Cloudinary.new({ cloud_name: cldObj.cloudName }); // eslint-disable-line no-undef
+
+                if (cldObj.domain !== 'res.cloudinary.com') {
+                    cldObj.video.widgetOptions.privateCdn = true;
+                    cldObj.video.widgetOptions.secureDistribution = cldObj.domain;
+                }
+                var cld = window.cldVideoPlayer.new({ cloud_name: cldObj.cloudName }); // eslint-disable-line no-undef
                 var player = cld.videoPlayer('cld-video-player' + (videoPlayerID ? '-' + videoPlayerID : ''), cldObj.video.widgetOptions);
                 player.source(cldObj.video.videoURL, {}).play();
                 player.transformation(cldObj.video.widgetOptions.transformations);
@@ -66,8 +82,12 @@ window.makeCloudinaryImagesResponsive = function () {
     var $cldEl = $('.cloudinary-data-container');
     var cloudinaryObj = $cldEl.data('cloudinary');
     if ($cldResponsiveImgTags && $cldResponsiveImgTags.length > 0) {
-        window.cldObj = window.cldObj || cloudinary.default.Cloudinary.new({cloud_name: cloudinaryObj.cloudName || cloudinaryObj}); // eslint-disable-line no-undef
-        window.cldObj.responsive(); // eslint-disable-line no-undef
+        if (window.cldObj) {
+            window.cldObj = window.cldObj;
+        } else if ( window.cloudinary && window.cloudinary.default) {
+            window.cldObj = window.cloudinary.default.Cloudinary.new({ cloud_name: cloudinaryObj.cloudName || cloudinaryObj });
+        }
+        window.cldObj && window.cldObj.responsive(); // eslint-disable-line no-undef
     }
 };
 
